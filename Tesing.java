2be722449d9210dd89bb6f0917780070b3412463
Yap Wen Qing <yapwenqing711@gmail.com>
2026-03-19 16:29:14 +0800

    Revert "Update Tesing.java"
    This reverts commit 80b9ff6752de6051740fec29ddd0a7162981ccaa.

--- Tesing.java
+++ Tesing.java
@@ -1,7 +1,5 @@
 public class Tesing {
     public static void main(String[] args) {
         System.out.println("This is edited in github");
-        System.out.println("This is also from git hub");
-        System.out.println("This is not from git");
     }
 }
